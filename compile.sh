1604bd60cb7c8831f8dda123e95acba2545c76b2
Martijn van Iersel <mvaniersel@gmail.com>
2021-01-23 19:20:35 +0100

    Improve compile script

--- compile.sh
+++ compile.sh
@@ -1,1 +1,4 @@
-dmd src/*.d src/helix/*.d src/helix/util/*.d src/allegro5/*.d src/helix/allegro/*.d -Isrc
+#!/bin/bash
+
+# -L/SUBSYSTEM:WINDOWS:4.0 prevents console window
+dmd src/*.d src/helix/*.d src/helix/util/*.d src/allegro5/*.d src/helix/allegro/*.d -Isrc -L/SUBSYSTEM:WINDOWS:4.0
